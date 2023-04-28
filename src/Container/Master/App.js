cafb34129d5b303606f65e2af7cdc30f5a5d45df
Angelo Di mauro <angelodimauro91@gmail.com>
2023-04-28 16:54:46 +0200

    esaurimenti

--- src/Container/Master/App.js
+++ src/Container/Master/App.js
@@ -1,22 +1,28 @@
-import ListActivity from '../Slave/ListActivity';
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import ListActivity from "../Slave/ListActivity";
+import AddTask from "../Slave/AddTask";
 
 function App() {
-
   const [data, setData] = useState([
-    {id: 1, name: "Angelo", completed: false},
-    {id: 2, name: "Angelo", completed: false},
-    {id: 3, name: "Angelo", completed: false}
+    { id: 1, name: "Attività 1", completed: false },
+    { id: 2, name: "Attività 2", completed: false },
+    { id: 3, name: "Attività 3", completed: false }
   ]);
 
   const removeItem = (id) => {
-    const updatedData = data.filter(item => item.id !== id);
+    const updatedData = data.filter((item) => item.id !== id);
     setData(updatedData);
-  }
+  };
+
+  const addTask = (newTask) => {
+    const task = { id: data.length + 1, name: newTask, completed: false };
+    setData([...data, task]);
+  };
 
   return (
-    <div className="App">
+    <div>
       <ListActivity data={data} onRemove={removeItem} />
+      <AddTask onAddTask={addTask} data={data} />
     </div>
   );
 }
