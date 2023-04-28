28e6ce99e3e44b5a542b558f95b95e064eea7049
Angelo Di mauro <angelodimauro91@gmail.com>
2023-04-28 14:28:02 +0200

    aggiungo il checkBox (da implementare la funzione)

--- src/Container/Slave/ListActivity.jsx
+++ src/Container/Slave/ListActivity.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import Checkbox from '@mui/material/Checkbox';
 const ListActivity = (props) => {
 
   const activityList = props.activityList;
+
+  const [checked, setChecked] = React.useState(true);
+
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
 
   return (
     <div>
@@ -10,6 +16,12 @@ const ListActivity = (props) => {
         {activityList.map((activity) => (
           <li key={activity.id}>
             {activity.title}
+
+            <Checkbox
+            checked={checked}
+            onChange={handleChange}
+            inputProps={{ 'aria-label': 'controlled' }}
+            />
           </li>
         ))}
       </ul>
