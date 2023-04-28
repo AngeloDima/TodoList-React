d236ecf3fba28545914488231d25b29a1b6af793
Angelo Di mauro <angelodimauro91@gmail.com>
2023-04-28 17:00:17 +0200

    checkBox

--- src/Container/Slave/ListActivity.jsx
+++ src/Container/Slave/ListActivity.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-
+import CheckBox from './CheckBox';
 const ListActivity = (props) => {
   return (
     <div>
       <ul>
         {props.data.map(item => (
           <li key={item.id}>
+            <CheckBox />
             {item.name}
             <button onClick={() => props.onRemove(item.id)}>Remove</button>
+
           </li>
         ))}
       </ul>
