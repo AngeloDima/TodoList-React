d186944f6e1e1564bf08e7903b03ee41dd2d5a76
Angelo Di mauro <angelodimauro91@gmail.com>
2023-04-29 08:30:11 +0200

    grafica per il bottone (distruttore)

--- src/Container/Slave/RemoveAll.jsx
+++ src/Container/Slave/RemoveAll.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
-
+import Box from '@mui/material/Box';
+import Grid from "@mui/material/Grid";
 
 const RemoveAll = ({ destroyData }) => {
   const Bye = () => {
@@ -9,16 +10,22 @@ const RemoveAll = ({ destroyData }) => {
   };
 
   return (
-    <div>
-      <Stack spacing={2} direction="row">
-        <Button variant="contained" onClick={Bye} style={{ backgroundColor: "red", color: "white" }}>
-          Cancella tutto
-        </Button>
-     </Stack>
-    </div>
-  )
-}
+    <Grid container spacing={2} justifyContent="center">
+      <Grid item xs={11} md={10}>
+        <Box sx={{ width: '100%' }}>
+          <Stack sx={{ width: '90%' }}>
+            <Button
+              variant="contained"
+              onClick={Bye}
+              sx={{ backgroundColor: 'red', color: 'white' }}
+            >
+              Cancella tutto
+            </Button>
+          </Stack>
+        </Box>
+      </Grid>
+    </Grid>
+  );
+};
 
-export default RemoveAll
-
-
+export default RemoveAll;
