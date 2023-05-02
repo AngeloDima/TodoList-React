e04b9fc4413b18ccfcb814246eb5dfb4b6fac7a4
Angelo Di mauro <angelodimauro91@gmail.com>
2023-05-02 13:17:22 +0200

    salvo con una richiesta POST alla API

--- src/Container/Master/App.js
+++ src/Container/Master/App.js
@@ -3,26 +3,60 @@ import ListActivity from "../Slave/ListActivity";
 import AddTask from "../Slave/AddTask";
 import RemoveAll from "../Slave/RemoveAll";
 import { v4 as uuidv4 } from "uuid";
+import axios from 'axios';
 
 function App() {
   // ARRAY
   const [data, setData] = useState([]);
 
+  console.log(data)
+
+
   // RIMUOVERE TASK
   const removeItem = (id) => {
-    const updatedData = data.filter((item) => item.id !== id);
-    setData(updatedData);
+    axios.delete(`https://my-json-server.typicode.com/AngeloDima/TodoList-React/task/${id}`)
+      .then(() => {
+        const updatedData = data.filter((item) => item.id !== id);
+        setData(updatedData);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   };
 
   // CREARE TASK
   const addTask = (newTask) => {
     const task = { id: uuidv4(), name: newTask, completed: false };
-    setData([...data, task]);
+    axios.post('https://my-json-server.typicode.com/AngeloDima/TodoList-React/task', task)
+      .then(response => {
+        setData([...data, response.data]);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   };
+  
+
+  // OTTENERE I DATI
+  useEffect(() => {
+    axios.get('https://my-json-server.typicode.com/AngeloDima/TodoList-React/task')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
 
   // ELIMINARE TUTTI I DATI
   const removeAll = () => {
-    setData([]);
+    axios.delete('https://my-json-server.typicode.com/AngeloDima/TodoList-React/task')
+      .then(() => {
+        setData([]);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   };
 
   return (
